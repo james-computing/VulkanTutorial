2b7482a3087731e5b8e56a9f7985de236dfcff3f
james <jamessimeao@outlook.com>
2026-03-19 13:25:04 -0300

    Make a method to get the vertex input binding description

--- khronos-version/include/vertex.hpp
+++ khronos-version/include/vertex.hpp
@@ -14,4 +14,6 @@ import vulkan_hpp;
 struct Vertex {
     glm::vec2 pos;
     glm::vec3 color;
+
+    static vk::VertexInputBindingDescription getBindingDescription();
 };
--- khronos-version/src/vertex.cpp
+++ khronos-version/src/vertex.cpp
@@ -1,2 +1,9 @@
 #include "../include/vertex.hpp"
 
+vk::VertexInputBindingDescription Vertex::getBindingDescription() {
+    return vk::VertexInputBindingDescription {
+        .binding = 0,
+        .stride = sizeof(Vertex),
+        .inputRate = vk::VertexInputRate::eVertex
+    };
+}
