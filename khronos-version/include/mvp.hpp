d1f231e7c7d59c23d521a46432ec0877fffbd3bd
james <jamessimeao@outlook.com>
2026-03-20 16:59:02 -0300

    Make struct alignment explicit

--- khronos-version/include/mvp.hpp
+++ khronos-version/include/mvp.hpp
@@ -3,7 +3,7 @@
 #include <glm/glm.hpp> // for vectors and matrices for computer graphics
 
 struct UniformBufferObject {
-    glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 proj;
+    alignas(16) glm::mat4 model;
+    alignas(16) glm::mat4 view;
+    alignas(16) glm::mat4 proj;
 };
